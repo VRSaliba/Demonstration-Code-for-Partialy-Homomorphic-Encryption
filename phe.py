46c71d7a24239052cc1b2a260c2d66b44a1e05f2
Vinicius Saliba <vinicius.saliba@hotmail.com>
2025-06-23 15:37:16 -0300

    Ajuste nos prints

--- phe.py
+++ phe.py
@@ -53,68 +53,78 @@ while opcao != 7:
     print()
     match opcao:
         case 1:
-            print("Multiplicando valor 1 pelo valor 2 usando RSA")
+            print("Multiplicando valor 1 pelo valor 2 usando RSA", '\n')
             
-            print("Valor 1:", valor1, '\n' "Valor 2:", valor2)
-            multiplicacao = valor1 * valor2
-            print("Valor da multiplicação: ", multiplicacao, '\n')
+            print("Valor 1: ", valor1, '\n' "Valor 2: ", valor2, '\n')
             
             valor1_rsa = cs_rsa.encrypt(valor1)
             valor2_rsa = cs_rsa.encrypt(valor2)
-            print("Valor 1 criptografado:", valor1_rsa.value, '\n' "Valor 2 criptografado:", valor2_rsa.value, '\n')
+            print("Valor 1 criptografado: ", valor1_rsa.value, '\n' "Valor 2 criptografado:", valor2_rsa.value, '\n')
             
-            multiplicacao_rsa = cs_rsa.decrypt(valor1_rsa * valor2_rsa)
-            print("Resultado descriptografado:", multiplicacao_rsa)
+            multiplicacao_rsa = valor1_rsa * valor2_rsa
+            multiplicacao = cs_rsa.decrypt(multiplicacao_rsa)
+            print("Resultado criptografado:", multiplicacao_rsa.value)
+            
+            print("Resultado descriptografado: ", multiplicacao, '\n')
+            
         case 2:
             print("Adicionando valor 1 ao valor 2 usando Paillier")
             
-            print("Valor 1:", valor1, '\n' "Valor 2:", valor2)
-            soma = valor1 + valor2
-            print("Valor da soma: ", soma, '\n')
+            print("Valor 1:", valor1, '\n' "Valor 2: ", valor2, '\n')
             
             valor1_pai = cs_pai.encrypt(valor1)
             valor2_pai = cs_pai.encrypt(valor2)
-            print("Valor 1 criptografado:", valor1_pai.value, '\n' "Valor 2 criptografado:", valor2_pai.value, '\n')
+            print("Valor 1 criptografado: ", valor1_pai.value, '\n' "Valor 2 criptografado:", valor2_pai.value, '\n')
             
-            soma_pai = cs_pai.decrypt(valor1_pai + valor2_pai)
-            print("Resultado descriptografado:", soma_pai)
+            soma_pai = valor1_pai + valor2_pai
+            soma = cs_pai.decrypt(soma_pai)
+            print("Resultado criptografado: ", soma_pai.value)
+            print("Resultado descriptografado: ", soma)
+            
         case 3:
-            print("Multiplicando valor 1 pelo valor de uma constante usando Okamoto-Uchiyama")
+            print("Multiplicando valor 1 pelo valor de uma constante usando Okamoto-Uchiyama", '\n')
             
+            print("Valor 1: ", valor1)            
             valor1_ouc = cs_ouc.encrypt(valor1)
-            print("Valor 1 criptografado:", valor1_ouc.value, '\n')
+            print("Valor 1 criptografado: ", valor1_ouc.value, '\n')
             
             print("Insira um valor para multiplicar: ")
-            multiplicacao_ouc = cs_ouc.decrypt(valor1_ouc * float(input()))
+            multiplicacao_const = valor1_ouc * float(input())
+            multiplicacao_ouc = cs_ouc.decrypt(multiplicacao_const)
+            
+            print("Resultado criptografado:", multiplicacao_const.value)
             print("Resultado descriptografado:", multiplicacao_ouc)
         case 4:
             print("Opera um XOR (OU exclusivo) entre o valor 1 e o valor 2 usando Goldwasser-Micali")
             
             valor1_gom = cs_gom.encrypt(valor1)
             valor2_gom = cs_gom.encrypt(valor2)
-            print("Valor 1 criptografado:", valor1_gom, '\n' "Valor 2 criptografado:", valor2_gom, '\n')
+            print("Valor 1 criptografado:", valor1_gom.value, '\n' "Valor 2 criptografado:", valor2_gom.value, '\n')
             
-            xor_gom = cs_gom.decrypt(valor1_gom ^ valor2_gom)
+            xor_gom = valor1_gom ^ valor2_gom
+            xor = cs_gom.decrypt(xor_gom)
             
-            print("Valor XOR entre valor 1 e valor 2: ", int(valor1) ^ int(valor2))
             print("Valor XOR entre valor 1 e valor 2 criptografados: ", xor_gom)
+            print("Valor XOR entre valor 1 e valor 2 descriptografados: ", xor)
+            
         case 5:
-            print("Gera outro valor cifrado do valor 1 usando Benaloh")
+            print("Gera outro cyphertext do valor 1 usando Benaloh")
                         
-            valor1_ben_original = cs_ben.encrypt(valor1)
-            print("Exemplo de valor criptografado usando o valor 1: ", valor1_ben_original.value)
+            print("Valor 1 descriptografado: ", valor1, '\n')
+            valor1_ben_original = cs_ben.encrypt(int(valor1))
+            print("Cyphertext antigo do valor 1: ", valor1_ben_original.value)
             
             valor1_ben_novo = cs_ben.regenerate_ciphertext(valor1_ben_original)
-            print("Novo valor criptografado usando o valor 1: ", valor1_ben_novo.value)
+            print("Cyphertext novo do valor 1: ", valor1_ben_novo.value, '\n')
             
-            print("Valor 1 descriptografado:", cs_ben.decrypt(valor1_ben_original))
-            print("Novo valor descriptografado:", cs_ben.decrypt(valor1_ben_novo))
+            print("Cyphertext antigo do valor 1 descriptografado:", cs_ben.decrypt(valor1_ben_original))
+            print("Cyphertext novo do valor 1 descriptografado:", cs_ben.decrypt(valor1_ben_novo))
         case 6:
             print("Insira o valor 1")
             valor1 = float(input())
             print("Insira o valor 2")
             valor2 = float(input())
         case 7:
-            print("Parando execucao")
+            print("Parando execução")
         case _:
-            print("Erro, favor inserir valor valido")
+            print("Erro, favor inserir valor válido")
